a434d93b8ef954d4a4f29ffac5b7f48b2ee50c4a
Avantika Kesarwani <avikesar2013@gmail.com>
2024-09-03 14:34:29 +0530

    scanning for directories

--- app/memory.py
+++ app/memory.py
@@ -1,44 +1,23 @@
-r'''import subprocess
-from parse import scan_path
-
-
-#function to automate mount point
-def mount(dump_file, mount_point="M:"):
-
-    command = f"memprocfs.exe -mount {mount_point} -device {dump_file} -forensic 1"
-    
-    try:
-        subprocess.run(command, check = True, shell = True, )
-        print("mounted successfully at {mount_point}")
-        return mount_point
-    
-    except subprocess.CalledProcessError as e:
-        print(f"failed to mount at drive {e}")
-    
-    except Exception as e:
-        print(f"An error occurred: {e}")
-    return None
-
-
-if __name__ == '__main__':
-
-    mem_dump_file = r"C:\Users\avike\OneDrive\Desktop\MemoryDump_Lab1.raw"
-
-    mounted = mount(mem_dump_file, "M:")
-
-    #if mount point is returned then scan the mounted directory 
-    if mounted:
-        scan_path(mounted)'''
-
 
 from parse import scan_path
 import memprocfs
 
+def scan_dir(vmm, directory =  "/"):
+    for entry in vmm.vfs.list(directory):
+        full_path = directory + "/" + entry.name
+        if entry.is_dir():
+            scan_dir(vmm, full_path)
+        else:
+            scan_path(full_path)
+
+
 vmm = memprocfs.Vmm(['-device', r'C:\Users\avike\OneDrive\Desktop\MemoryDump_Lab1.raw', '-forensic', '1'])
 
-for file in vmm.vfs.list("/"):
-    scan_path(file)
+scan_dir(vmm)
 
+
+
+    
 
 
 
