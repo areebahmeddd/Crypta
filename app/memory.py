9e9f21442a32916c0292e991ab539b533149846b
Avantika Kesarwani <avikesar2013@gmail.com>
2024-09-03 15:03:02 +0530

    minor changes

--- app/memory.py
+++ app/memory.py
@@ -6,7 +6,7 @@ import memprocfs
 
 vmm = memprocfs.Vmm(['-device', r'C:\Users\avike\OneDrive\Desktop\MemoryDump_Lab1.raw', '-forensic', '1'])
 
-for file in vmm.vfs,list("/"):
+for file in vmm.vfs.list("/"):
     scan_path(file)
 
 
