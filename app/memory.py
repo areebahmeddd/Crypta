24ab7ba99ba8999acf5a4fa543aad0f00a34aa9e
Avantika Kesarwani <avikesar2013@gmail.com>
2024-09-03 15:01:36 +0530

    minor changes

--- app/memory.py
+++ app/memory.py
@@ -1,19 +1,13 @@
 
 from parse import scan_path
+import json
 import memprocfs
-
-def scan_dir(vmm, directory =  "/"):
-    for entry in vmm.vfs.list(directory):
-        full_path = directory + "/" + entry.name
-        if entry.is_dir():
-            scan_dir(vmm, full_path)
-        else:
-            scan_path(full_path)
 
 
 vmm = memprocfs.Vmm(['-device', r'C:\Users\avike\OneDrive\Desktop\MemoryDump_Lab1.raw', '-forensic', '1'])
 
-scan_dir(vmm)
+for file in vmm.vfs,list("/"):
+    scan_path(file)
 
 
 
