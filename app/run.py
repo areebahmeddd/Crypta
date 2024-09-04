3bb975868efb666136d5a0a5133f24a7ef283926
Avantika Kesarwani <avikesar2013@gmail.com>
2024-09-05 03:32:55 +0530

    req containing files ( :/ )

--- app/run.py
+++ app/run.py
@@ -6,6 +6,9 @@
 
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
+from typing import Annotated
+from fastapi import FastAPI, File, UploadFile
+
 
 app = FastAPI()
 
@@ -22,8 +25,18 @@ app.add_middleware(
 
 #upload route
 @app.post("/api/upload")
-async def upload_file():
-    return {"message" : "hello server"}
+async def upload_file(files: list[UploadFile] = File(...), rulesFile: UploadFile = File(...)):
+    file_names = [file.filename for file in files]
+    yara_file_name = rulesFile.filename
+    return {
+        "uploaded_files": file_names,
+        "yara_file": yara_file_name
+    }
+
+
+
+
+
 
 
 
