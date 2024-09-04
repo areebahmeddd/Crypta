d70feb692f669a136c789ea052426e26f2f088f7
Avantika Kesarwani <avikesar2013@gmail.com>
2024-09-05 01:52:17 +0530

    testing update route

--- app/run.py
+++ app/run.py
@@ -1,4 +1,31 @@
-from parse import scan_path
+#from parse import scan_path
 
-if __name__ == '__main__':
-    scan_path('logs')
+#if __name__ == '__main__':
+    #scan_path('logs')'''
+
+
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+
+app = FastAPI()
+
+
+# Add CORS middleware
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"], # Allows all origins
+    allow_credentials=True,
+    allow_methods=["*"], # Allows all methods
+    allow_headers=["*"], # Allows all headers
+)
+
+
+#upload route
+@app.post("/api/upload")
+async def upload_file():
+    return {"message" : "hello server"}
+
+
+
+
+
