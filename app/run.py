71656d766e3269bc95dc855797e40b70b7fe9140
Areeb Ahmed <135235925+areebahmeddd@users.noreply.github.com>
2024-09-05 12:19:19 +0530

    http port fix

--- app/run.py
+++ app/run.py
@@ -5,21 +5,21 @@ from fastapi.middleware.cors import CORSMiddleware
 app = FastAPI()
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],
-    allow_methods=["*"],
-    allow_headers=["*"]
+    allow_origins=['*'],
+    allow_methods=['*'],
+    allow_headers=['*']
 )
 
-@app.post("/api/upload")
+@app.post('/api/upload')
 async def upload(files: list[UploadFile] = File(...), rulesFile: UploadFile = File(...)):
     file_names = [file.filename for file in files]
     yara_file_name = rulesFile.filename
     return {
-        "uploaded_files": file_names,
-        "yara_file": yara_file_name
+        'uploaded_files': file_names,
+        'yara_file': yara_file_name
     }
 
-@app.post("/api/detect")
+@app.post('/api/detect')
 async def detect():
     pass
 
